831873962597e2e29f636b2e64edfa6eb87cafa8
dayjyun <kbarriosdev@gmail.com>
2023-06-01 12:34:53 -0500

    Completed connection between Airport Service and Controller

--- src/main/java/com/skylight/controllers/AirportController.java
+++ src/main/java/com/skylight/controllers/AirportController.java
@@ -1,9 +1,30 @@
 package com.skylight.controllers;
 
+import com.skylight.models.Airport;
+import com.skylight.services.AirportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/airports")
 public class AirportController {
+
+   @Autowired
+   private AirportService airportService;
+
+   @GetMapping(path = "")
+   public List<Airport> getAirports() {
+      return airportService.getAllAirports();
+   }
+
+   @GetMapping(path = "/{airportId}")
+   public Optional<Airport> getAirportById(@PathVariable Long airportId) {
+      return airportService.getAirportById(airportId);
+   }
 }
--- src/main/java/com/skylight/services/AirportService.java
+++ src/main/java/com/skylight/services/AirportService.java
@@ -1,4 +1,27 @@
 package com.skylight.services;
 
+import com.skylight.models.Airport;
+import com.skylight.repositories.AirportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AirportService {
+
+    private AirportRepository airportRepository;
+
+    @Autowired
+   public void setAirportRepository(AirportRepository airportRepository) {
+       this.airportRepository = airportRepository;
+    }
+
+    public List<Airport> getAllAirports() {
+       return airportRepository.findAll();
+    }
+
+    public Optional<Airport> getAirportById(Long airportId) {
+       return airportRepository.findById(airportId);
+    }
 }
