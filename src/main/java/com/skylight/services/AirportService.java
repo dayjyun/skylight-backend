2b052b480db4b99808c69b722d1c02543c8a69be
dayjyun <kbarriosdev@gmail.com>
2023-06-01 12:56:01 -0500

    Completed endponts for Airports in Service and Controller

--- src/main/java/com/skylight/services/AirportService.java
+++ src/main/java/com/skylight/services/AirportService.java
@@ -1,5 +1,6 @@
 package com.skylight.services;
 
+import com.skylight.exceptions.NotFoundException;
 import com.skylight.models.Airport;
 import com.skylight.repositories.AirportRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,12 +17,39 @@ public class AirportService {
    public void setAirportRepository(AirportRepository airportRepository) {
        this.airportRepository = airportRepository;
     }
-
-    public List<Airport> getAllAirports() {
-       return airportRepository.findAll();
+    
+   /**
+    * allAirports returns a list of all airports in the database
+    * A NotFoundException is thrown if no airports are found
+    * @return List of all airports
+    */
+   public List<Airport> getAllAirports() {
+      // Create a list of airports
+       List<Airport> allAirports = airportRepository.findAll();
+       // Check if the list is empty
+       if(allAirports.isEmpty()) {
+          // Throw an error if not airports are found
+          throw new NotFoundException("No airports found");
+       }
+       // Return the list of all airports if not empty
+       return allAirports;
     }
 
-    public Optional<Airport> getAirportById(Long airportId) {
-       return airportRepository.findById(airportId);
-    }
+   /**
+    * getAirportById returns an airport by its id
+    * A NotFoundException is thrown if no airport is found
+    * @param airportId is the targeted airport
+    * @return Airport
+    */
+   public Optional<Airport> getAirportById(Long airportId) {
+      // Create an optional of an airport
+      Optional<Airport> airport = airportRepository.findById(airportId);
+      // Check if the optional is empty
+      if(airport.isEmpty()) {
+         // Throw an error if no airport is found
+         throw new NotFoundException("No airport found");
+      }
+      // Return the airport data
+      return airport;
+   }
 }
