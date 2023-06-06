f6b770b99b107650e165fbf23faf60514a314473
dayjyun <kbarriosdev@gmail.com>
2023-06-06 10:17:17 -0500

    Updated Security Configuration to set up frontend access

--- src/main/java/com/skylight/security/SecurityConfiguration.java
+++ src/main/java/com/skylight/security/SecurityConfiguration.java
@@ -18,6 +18,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true) // this security configuration applies to entire app configuration
@@ -86,6 +88,19 @@ public class SecurityConfiguration {
           .and().csrf().disable(); // connects front/back end if they're on different servers
       http.addFilterBefore(authJwtRequestFilter(), UsernamePasswordAuthenticationFilter.class); // added for JWT login
       return http.build();
+   }
+
+   @Bean
+   public WebMvcConfigurer corsConfigurer() {
+      return new WebMvcConfigurer() {
+         @Override
+         public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                    .allowedHeaders("*")
+                    .allowedOrigins("*");
+         }
+      };
    }
 
    /**
--- src/main/java/com/skylight/services/UserService.java
+++ src/main/java/com/skylight/services/UserService.java
@@ -20,7 +20,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.Objects;
-import java.util.Optional;
 
 @Service
 public class UserService {
