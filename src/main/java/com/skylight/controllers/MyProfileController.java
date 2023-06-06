e0d8738ca04063a8d025a1e6125f28de5054094d
dayjyun <kbarriosdev@gmail.com>
2023-06-05 22:07:11 -0500

    Updated docstrings on MyProfileController

--- src/main/java/com/skylight/controllers/MyProfileController.java
+++ src/main/java/com/skylight/controllers/MyProfileController.java
@@ -17,36 +17,52 @@ public class MyProfileController {
    @Autowired
    private MyProfileService myProfileService;
 
-   // Functionality: Returns logged-in user’s account (Private)
-   // Path: /api/myProfile
+   /**
+    * Functionality: Returns logged-in user’s account (Private)
+    * Path: /api/myProfile
+    * @return Logged-in user's data
+    */
    @GetMapping("")
    public User getLoggedInUser() {
       return MyProfileService.getLoggedInUser();
    }
 
-   // Functionality: Edit user account (Private)
-   // Path: /api/myProfile
+   /**
+    * Functionality: Edit user account (Private)
+    * Path: /api/myProfile
+    * @param updateBody is the updated information for the logged-in user
+    * @return updated user data
+    */
    @PutMapping("")
    public User updateMyProfile(@RequestBody @Valid User updateBody) {
       return myProfileService.updateMyProfile(updateBody);
    }
 
-   // Functionality: Returns a list of flights the user booked	(Private)
-   // Path: /api/myProfile/myTickets
+   /**
+    * Functionality: Returns a list of flights the user booked	(Private)
+    * Path: /api/myProfile/myFlights
+    * @return List of tickets the user has booked
+    */
    @GetMapping("/myTickets")
    public List<Ticket> getMyTickets() {
       return myProfileService.getMyTickets();
    }
 
-   // Functionality: User submits request to become a pilot (Private)
-   // Path: /api/myProfile/flyTheSkies
+   /**
+    * Functionality: User submits request to become a pilot (Private)
+    * Path: /api/myProfile/flyTheSkies
+    * @return updated user data
+    */
    @PutMapping("/flyTheSkies")
    public User flyTheSkies() {
       return myProfileService.flyTheSkies();
    }
 
-   //  Functionality: Returns a list of flight the admin has submitted	(Private)
-   // Path: /api/myProfile/air
+   /**
+    * Functionality: Returns a list of flight the admin has submitted	(Private)
+    * Path: /api/myProfile/air
+    * @return List of flights the user has scheduled
+    */
    @GetMapping("/air")
    public List<Flight> getScheduledFlights() {
       return myProfileService.getScheduledFlights();
